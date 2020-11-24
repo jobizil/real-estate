d65ec16c64bcd0aca47e35ff8bc517b3cfc26c21
Job Ugbem <ugbemjob57@gmail.com>
2020-11-24 23:59:29 +0100

    Listings URLs & Template #18

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import  Listing
 
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    """Displays a table of items in the admin page """
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')    # Set clickable items
+    list_filter = ('realtor',)      #Set filter items
+    list_editable = ('is_published',)       #Set Checkbox on bool
+    search_fields = ('title', 'address', 'city', 'price', 'zipcode', 'description')     #Set Search Option
+    list_per_page = 20    #pagination 
+
+admin.site.register(Listing, ListingAdmin)
