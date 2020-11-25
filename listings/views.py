72989bdc5219689c993088a3ad5633227178de1f
Job Ugbem <ugbemjob57@gmail.com>
2020-11-25 01:41:58 +0100

    Display listings template, paginate and set published status #19

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-# Gets item listings in db
-def listings(request):
-    return render(request, 'listings/listings.html')
+from .models import Listing
 
-# Gets items listing by its id
-def listing(request):
+
+def index(request):
+    """ Gets item listings in db """
+    listings = (
+    Listing.objects
+        ).order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)      # Set Pagination
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = { 'listings':paged_listings }
+
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
+    """ Gets items listing by its id """
     return render(request, 'listings/listing.html')
 
-# Provides functionality to perform search
+
 def search(request):
+    """ Provides functionality to perform search """
     return render(request, 'listings/search.html')
