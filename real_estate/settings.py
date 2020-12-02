c061bf4abd6abfc33127a7f99488b922bd834daa
Job Ugbem <ugbemjob57@gmail.com>
2020-12-02 11:15:57 +0100

    Added local settings import

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -149,9 +149,11 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USER = 'quilltech57@gmail.com'
-EMAIL_HOST_PASSWORD = 'HELLOworld081++'
+EMAIL_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
-
-print(f"EMAIL_HOST = {EMAIL_HOST}")
+try:
+    from .local_settings import *
+except ImportError:
+    pass
